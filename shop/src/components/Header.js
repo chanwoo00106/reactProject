ce0558f6cc9a6800915fb76d8f7d3b40625c567c
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-23 23:41:21 +0900

    Update Header.js
    오류 제거

--- shop/src/components/Header.js
+++ shop/src/components/Header.js
@@ -10,8 +10,8 @@ const Header = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                <Nav.Link> <Link to="/">Home</Link> </Nav.Link>
-                <Nav.Link> <Link to="/detail">Detail</Link> </Nav.Link>
+                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                <Nav.Link as={Link} to="/detail">Detail</Nav.Link>
                 <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
