b6fa8eefa6bc618e7cbcc47ccb9e90480a53d504
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-23 22:37:47 +0900

    Create Header.js

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,35 +1,19 @@
 import React from 'react';
-import {Navbar, Container, Nav, NavDropdown, Button} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import './App.css';
 import Card from './components/Card';
-import {Link, Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Detail from './components/Detail';
 import data from './db/data';
+import Header from './components/Header';
+
 
 function App() {
 
   return (
     <div className="App">
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">Shop</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link> <Link to="/">Home</Link> </Nav.Link>
-              <Nav.Link> <Link to="/detail">Detail</Link> </Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      
+      <Header />
+
       <Switch>
         <Route exact path="/">
           <div className="background">
