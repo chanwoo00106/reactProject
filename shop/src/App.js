bb707320c664f1368920cc965a90c9888b81a79a
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-23 17:40:34 +0900

    bootstrap

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,8 +1,39 @@
+import React from 'react';
+import {Navbar, Container, Nav, NavDropdown, Button} from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div className="background">
+        <h1>20% Season Off</h1>
+        <p>
+          This is a sample hero unit, a simple Jumbotron-style components for calling extra attention to featured content or information.
+        </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </div>
 
     </div>
   );
