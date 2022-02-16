7fb3fcae3cc863c1ea46d368aef5979c192e1e4c
변찬우 <chan00106@naver.com>
2022-02-16 23:52:23 +0900

    what is that

--- wordList/src/App.js
+++ wordList/src/App.js
@@ -1,9 +1,9 @@
-import Header from './components/Header';
-import DayList from './components/DayList';
-import Day from './components/Day';
-import CreateWord from './components/CreateWord'
-import EmptyPage from './components/EmptyPage';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Header from "./components/Header";
+import DayList from "./components/DayList";
+import Day from "./components/Day";
+import CreateWord from "./components/CreateWord";
+import EmptyPage from "./components/EmptyPage";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
